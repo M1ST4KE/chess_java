7166e0cd45c2b8f7038ff6f9e4b9989d1024f0c0
irasi11 <dominikrap@gmail.com>
2019-06-20 22:35:25 +0200

    Client

--- client/src/main/java/pl/chessonline/client/model/Alliance.java
+++ client/src/main/java/pl/chessonline/client/model/Alliance.java
@@ -1,4 +1,40 @@
 package pl.chessonline.client.model;
 
 public enum Alliance {
+    WHITE {
+        @Override
+        public int getDirection(){
+            return -1;
+        }
+
+        @Override
+        public boolean isWhite() {
+            return true;
+        }
+
+        @Override
+        public boolean isBlack() {
+            return false;
+        }
+    },
+    BLACK {
+        @Override
+        public int getDirection(){
+            return 1;
+        }
+
+        @Override
+        public boolean isWhite() {
+            return false;
+        }
+
+        @Override
+        public boolean isBlack() {
+            return true;
+        }
+    };
+
+    public abstract int getDirection();
+    public abstract boolean isWhite();
+    public abstract boolean isBlack();
 }
--- client/src/main/java/pl/chessonline/client/model/Piece.java
+++ client/src/main/java/pl/chessonline/client/model/Piece.java
@@ -4,10 +4,17 @@ import java.util.Collection;
 public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
     Piece(final int piecePosition, final Alliance pieceAlliance) {
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        //TODO MORE WORK
+        this.isFirstMove = false;
+    }
+
+    public  boolean isFirstMove(){
+        return this.isFirstMove;
     }
 
     public Alliance getPieceAlliance() {

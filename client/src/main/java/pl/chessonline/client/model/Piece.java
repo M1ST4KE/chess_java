116b969a42f7440bcd18ac167a2a7fff7e45a45e
irasi11 <dominikrap@gmail.com>
2019-06-20 18:16:02 +0200

    Client

---
+++ client/src/main/java/pl/chessonline/client/model/Piece.java
@@ -0,0 +1,19 @@
+package pl.chessonline.client.model;
+import java.util.Collection;
+
+public abstract class Piece {
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+
+    Piece(final int piecePosition, final Alliance pieceAlliance) {
+        this.piecePosition = piecePosition;
+        this.pieceAlliance = pieceAlliance;
+    }
+
+    public Alliance getPieceAlliance() {
+        return this.pieceAlliance;
+    }
+
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
+}
+

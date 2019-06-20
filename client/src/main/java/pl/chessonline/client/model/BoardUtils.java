e9178e9a4cd431964148e6230275b3aa92b666cc
irasi11 <dominikrap@gmail.com>
2019-06-20 19:26:55 +0200

    Client

--- client/src/main/java/pl/chessonline/client/model/BoardUtils.java
+++ client/src/main/java/pl/chessonline/client/model/BoardUtils.java
@@ -1,14 +1,26 @@
 package pl.chessonline.client.model;
 
 public class BoardUtils {
-    public static final boolean[] FIRST_COLUMN  = null;
-    public static final boolean[] SECOND_COLUMN = null;
-    public static final boolean[] SEVENTH_COLUMN = null;
-    public static final boolean[] EIGHT_COLUMN = null;
 
+    public static final boolean[] FIRST_COLUMN  = initColumn(0);
+    public static final boolean[] SECOND_COLUMN = initColumn(1);
+    public static final boolean[] SEVENTH_COLUMN = initColumn(6);
+    public static final boolean[] EIGHT_COLUMN = initColumn(7);
+
+    public static final int NUM_TILES = 64;
+    public static final int NUM_TILES_PER_ROW = 8;
 
     private BoardUtils() {
         throw new RuntimeException("You cann't instance me!");
+    }
+
+    private static boolean[] initColumn(int columnN) {
+        final boolean[] column = new boolean[NUM_TILES];
+        do {
+            column[columnN] = true;
+            columnN += NUM_TILES_PER_ROW;
+        } while (columnN < NUM_TILES);
+        return column;
     }
 
     public static boolean isValidTileCoordinate(int coordinate){
--- client/src/main/java/pl/chessonline/client/model/Move.java
+++ client/src/main/java/pl/chessonline/client/model/Move.java
@@ -1,4 +1,34 @@
 package pl.chessonline.client.model;
 
-public class Move {
+public abstract class Move {
+    final Board board;
+    final Piece movePiece;
+    final int destinationCoordinate;
+
+    private Move(final Board board, final Piece movePiece, final int destinationCoordinate){
+        this.board = board;
+        this.destinationCoordinate = destinationCoordinate;
+        this.movePiece = movePiece;
+    }
+
+    public static final class MajorMove extends Move {
+
+        public MajorMove(final Board board, final Piece movePiece, final int destinationCoordinate) {
+            super(board, movePiece, destinationCoordinate);
+        }
+
+    }
+
+    public static final class AttackMove extends Move {
+
+        final Piece attackPiece;
+        public AttackMove(final Board board, final Piece movePiece,
+                   final int destinationCoordinate, final Piece attackPiece) {
+            super(board, movePiece, destinationCoordinate);
+            this.attackPiece = attackPiece;
+        }
+
+
+
+    }
 }
